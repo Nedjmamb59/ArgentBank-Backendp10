f9fc428d1753e21f068cca5c4020839a3d50b797
Papa Mamadou Mbaye <mistermbaye@MacBook-Pro-de-Papa.local>
2024-11-04 09:41:59 +0100

    ajout assets

---
+++ frontend/src/assets/data/features.jsx
@@ -0,0 +1,27 @@
+import ChatIcon from '../img/icon-chat.webp'
+import MoneyIcon from '../img/icon-money.webp' ;
+import SecurityIcon from '../img/icon-security.webp'
+
+
+const features = [
+    {
+        image: ChatIcon,
+        alt: 'Chat Icon',
+        title: 'You are our #1 priority',
+        content: 'Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.',
+    },
+    {
+        image: MoneyIcon,
+        alt: 'Money Icon',
+        title: 'More savings means higher rates',
+        content: 'The more you save with us, the higher your interest rate will be!',
+    },
+    {
+        image: SecurityIcon,
+        alt: 'Security Icon',
+        title: 'Security you can trust',
+        content: 'We use top of the line encryption to make sure your data and money is always safe.',
+    }
+];
+
+export default features;
